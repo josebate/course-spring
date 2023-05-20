48d19500b069e8fc77791848abe19568c8c89a99
José Bate <nb29347@novabase.pt>
2023-05-20 17:50:53 +0200

    SECTION 9 DONE

--- Master Spring Boot 3 & Spring Framework 6 with Java/full-stack/01-frontend/todo-app/src/App.js
+++ Master Spring Boot 3 & Spring Framework 6 with Java/full-stack/01-frontend/todo-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LearningComponent from './components/learning-examples/LearningComponent'
 
 function App() {
   return (
     <div className="App">
-      My Todo Application
+        <LearningComponent/>
     </div>
   );
 }
