db3c9baed4d68f1ee6159c402dc4bc9370b287d3
rafaelbate <rafaelbate>
2023-07-16 18:31:57 +0100

    restructured

--- Master Spring Boot 3 & Spring Framework 6 with Java/full-stack/01-frontend/todo-app/src/components/counter/ResetCounter.jsx
+++
@@ -1,7 +0,0 @@
-export default function ResetCounter({resetMethod}){
-    return(
-        <div>
-            <button className="resetButton button" onClick={resetMethod}>Reset</button>
-        </div>
-    )
-}
